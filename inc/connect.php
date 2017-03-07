38fa2b501d302e881ebe538856351998a45ab385
piyarat <piyarat.tharaporn@gmail.com>
2017-03-07 10:09:41 +0700

    remove secure key
    remove secure key for projecttalent
    projectt_rev","123456","projectt_revsys2

--- inc/connect.php
+++ inc/connect.php
@@ -1,5 +1,5 @@
 <?php
-$con=mysqli_connect("localhost","projectt_rev","123456","projectt_revsys2");
+$con=mysqli_connect("localhost","","","");
 // Check connection
 if (mysqli_connect_errno()) {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
